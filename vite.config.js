70288d49a3ce526a86a7d32ee693647a22cde146
Christopher Carroll Smith <chriscarrollsmith@gmail.com>
2024-08-27 20:46:50 -0400

    fix: add base to Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    historyApiFallback: {
-      rewrites: [
-        { from: /./, to: '/index.html' },
-      ],
-    },
+    historyApiFallback: true,
   },
+  base: './'
 });
