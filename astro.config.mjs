cc466dea0e26536dbcc1db4becd4121c4f3256c2
chriscarrollsmith <chriscarrollsmith@gmail.com>
2025-11-05 21:08:19 -0500

    Basic SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://christophercarrollsmith.com',
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   vite: {
     ssr: {
       noExternal: ['react-helmet-async', '@formspree/react', 'convertkit-react', 'react-calendly']
