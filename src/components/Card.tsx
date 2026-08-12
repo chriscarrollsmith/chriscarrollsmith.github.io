f81aeb40ac08dc13800b431547e9dec3ad60b2da
chriscarrollsmith <christopher.smith@promptlytechnologies.com>
2026-08-12 17:28:09 -0400

    Adjust font size for readability

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -24,7 +24,7 @@ const Card: React.FC<CardProps> = ({ project }) => {
           </div>
         )}
         <div className={`body-section ${project.img ? 'with-image' : ''}`}>
-          <p>{project.description}</p>
+          <p className="small">{project.description}</p>
           <a
             href={project.url}
             className="button"
